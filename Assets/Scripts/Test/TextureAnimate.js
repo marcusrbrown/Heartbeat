2d029a95de8e6bc2a66681fef8a5e1263b8c830e
JTwice <jjohnstongames@gmail.com>
2013-01-27 15:16:12 -0700

    Updated texture animate as far as I could get it

--- Assets/Scripts/Test/TextureAnimate.js
+++ Assets/Scripts/Test/TextureAnimate.js
@@ -1,19 +1,35 @@
     var frames : Texture[];
     var framesPerSecond: float = 10;
-    var framesSlow: float = 10;
-    var framesNormal: float = 10;
+    var framesSlow: float = 15;
+    var framesNormal: float = 22;
+    var framesFast : float = 29;
+    var sizeSmall : float;
+    var sizeNormal : float;
+    var sizeLarge : float;
+    
+    private var currentFrames : float = framesNormal;
+    private var currentSize : float = sizeNormal;
      
     function Update() {
-        var index : int = (Time.time * framesPerSecond) % frames.Length;
+        var index : int = (Time.time * currentFrames) % frames.Length;
         renderer.material.mainTexture = frames[index];
+        //need to stretch the z of this plane over time to equal currentsize whether that be lower or higher
     }
     
-function HeartSlowOn()
+function HeartSlow()
 {
-framesPerSecond = framesSlow;
+currentFrames = framesSlow;
+currentSize = sizeSmall;
 }
 
-function HeartSlowOff()
+function HeartNormal()
 {
-framesPerSecond = framesNormal;
+currentFrames = framesNormal;
+currentSize = sizeNormal;
+}
+
+function HeartFast()
+{
+currentFrames = framesFast;
+currentSize = sizeLarge;
 }
