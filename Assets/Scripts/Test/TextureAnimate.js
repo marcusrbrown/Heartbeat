86301377e81f016d98e31ca2867adcc41963d714
JTwice <jjohnstongames@gmail.com>
2013-01-27 15:55:36 -0700

    Updated ppening... added music

--- Assets/Scripts/Test/TextureAnimate.js
+++ Assets/Scripts/Test/TextureAnimate.js
@@ -6,14 +6,32 @@
     var sizeSmall : float;
     var sizeNormal : float;
     var sizeLarge : float;
+    var titleScreen : boolean = false;
+   var stopAnimatingVar : boolean = false;
     
     private var currentFrames : float = framesNormal;
     private var currentSize : float = sizeNormal;
      
     function Update() {
+
+        if(titleScreen)
+        {
+
+        //the hackiest hack that ever hacked
+   			if(stopAnimatingVar==false)
+   			{
+           var index2 : int = (Time.time * framesPerSecond) % frames.Length;     
+            renderer.material.mainTexture = frames[index2];
+            if(index2==1)
+            {
+            stopAnimatingVar=true;
+            }
+            }
+        }else{
         var index : int = (Time.time * currentFrames) % frames.Length;
         renderer.material.mainTexture = frames[index];
         //need to stretch the z of this plane over time to equal currentsize whether that be lower or higher
+    }
     }
     
 function HeartSlow()
