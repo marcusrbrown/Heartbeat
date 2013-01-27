143984ef40a13398166f88cd36ef22f54ef85c08
Marcus R. Brown <mrbrown@precision-mojo.com>
2013-01-26 17:29:21 -0700

    Implement hit detection on heartbeat pings.

--- Assets/Scripts/Game/Metagame.cs
+++ Assets/Scripts/Game/Metagame.cs
@@ -1,15 +1,50 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Metagame : MonoBehaviour
 {
+    private enum ReceiverState
+    {
+        // The receiver is registered and active.
+        Active,
+
+        // The receiver was detected by a ping.
+        Detected,
+    }
+
     public GameObject player;
     public GameObject enemies;
     public GameObject items;
 
     private Heartbeat heartbeat_;
+    private Dictionary<PingReceiver, ReceiverInfo> receivers_ = new Dictionary<PingReceiver, ReceiverInfo>();
+
+    public void RegisterPingReceiver(PingReceiver receiver)
+    {
+        receivers_.Add(receiver, new ReceiverInfo(receiver));
+    }
+
+    public void UnregisterPingReceiver(PingReceiver receiver)
+    {
+        receivers_.Remove(receiver);
+    }
 
     public void CheckPulseCollisions(Vector2 pulseCenter, float pulseRadius)
     {
+        // Look at all ping receivers that are active, but not already detected.
+        var activeReceivers = receivers_.Values.Where(r => r.State == ReceiverState.Active);
+
+        foreach (ReceiverInfo active in activeReceivers)
+        {
+            PingReceiver receiver = active.Receiver;
+
+            if (IsPointInCircle(receiver.GetPingPoint(), pulseCenter, pulseRadius))
+            {
+                receiver.Ping();
+                active.State = ReceiverState.Detected;
+            }
+        }
     }
 
     private void Awake()
@@ -34,4 +69,29 @@ public class Metagame : MonoBehaviour
         heartbeat_ = this.player.GetComponent<Heartbeat>();
         heartbeat_.SetMetagame(this);
     }
+
+    private bool IsPointInCircle(Vector2 testPoint, Vector3 center, float radius)
+    {
+        float xDistance = testPoint.x - center.x;
+        float yDistance = testPoint.y - center.y;
+        float distanceSq = (xDistance * xDistance) + (yDistance * yDistance);
+
+        return distanceSq < (radius * radius);
+    }
+
+    #region ReceiverInfo nested class
+
+    class ReceiverInfo
+    {
+        public readonly PingReceiver Receiver;
+        public ReceiverState State;
+
+        internal ReceiverInfo(PingReceiver receiver)
+        {
+            Receiver = receiver;
+            State = ReceiverState.Active;
+        }
+    }
+
+    #endregion
 }
--- Assets/Scripts/Game/PingReceiver.cs
+++ Assets/Scripts/Game/PingReceiver.cs
@@ -2,8 +2,46 @@ using UnityEngine;
 
 public class PingReceiver : MonoBehaviour
 {
+    private Metagame metagame_;
+
+    public Vector2 GetPingPoint()
+    {
+        return new Vector2(this.transform.position.x, this.transform.position.z);
+    }
+
+    public void Ping()
+    {
+        OnPing();
+    }
+
     protected virtual void OnPing()
     {
-        Debug.Log(gameObject.name + ": Ping received!");
+        string debugString = string.Format("{0} ({1}, {2}): Ping received!", gameObject.name, GetPingPoint().x, GetPingPoint().y);
+
+        Debug.Log(debugString);
+    }
+
+    private void Awake()
+    {
+        GameObject metagameObject = GameObject.FindGameObjectWithTag("Metagame");
+
+        if ((metagameObject == null) || ((metagame_ = metagameObject.GetComponent<Metagame>()) == null))
+        {
+            Debug.LogError("Couldn't locate a GO with the \"Metagame\" tag or it's missing the Metagame script.");
+            this.enabled = false;
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        // Register ourselves with the metagame as a ping receiver.
+        metagame_.RegisterPingReceiver(this);
+    }
+
+    private void OnDestroy()
+    {
+        // Unregister ourselves as a ping receiver.
+        metagame_.UnregisterPingReceiver(this);
     }
 }
