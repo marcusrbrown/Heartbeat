21ae076dfa8be43bb5a6ad3d1056d94d1a95265d
JTwice <jjohnstongames@gmail.com>
2013-01-27 05:49:03 -0700

    Changed a script name for later and added the possibility to slow down the animated texture

--- Assets/Scripts/Test/TextureAnimate.js
+++ Assets/Scripts/Test/TextureAnimate.js
@@ -1,7 +1,19 @@
     var frames : Texture[];
     var framesPerSecond: float = 10;
+    var framesSlow: float = 10;
+    var framesNormal: float = 10;
      
     function Update() {
         var index : int = (Time.time * framesPerSecond) % frames.Length;
         renderer.material.mainTexture = frames[index];
     }
+    
+function HeartSlowOn()
+{
+framesPerSecond = framesSlow;
+}
+
+function HeartSlowOff()
+{
+framesPerSecond = framesNormal;
+}
