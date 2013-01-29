215237e9019c2d2a0c5707275828d0337a38fd8a
JTwice <jjohnstongames@gmail.com>
2013-01-28 23:36:20 -0700

    Fixed git weirdness

--- Assets/Scripts/Test/Score.js
+++ Assets/Scripts/Test/Score.js
@@ -2,11 +2,7 @@ public var scoreText : int = 1;
 
 function yay() 
 {
-<<<<<<< HEAD
 scoreText+=Random.Range(60, 80);
-=======
-scoreText+=Random.Range(60,80);
->>>>>>> Checked in score thing
 }
 
 function Update()
