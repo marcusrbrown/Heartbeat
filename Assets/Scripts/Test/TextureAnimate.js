ff042c029a57b1a9501c7848e85d5ea41478b740
JTwice <jjohnstongames@gmail.com>
2013-02-01 23:20:56 -0700

    Updated heart rate and the texture animated function with comments and notes on what code needs to be added

--- Assets/Scripts/Test/TextureAnimate.js
+++ Assets/Scripts/Test/TextureAnimate.js
@@ -1,26 +1,43 @@
     var frames : Texture[];
     var framesPerSecond: float = 10;
+    
+   //Used for faint heart rate
     var framesSlow: float = 15;
+    var yStretchSlow : float;
+    var yOffsetSlow : float;
+
+    //Used for normal heart rate
     var framesNormal: float = 22;
+    var yStretchNormal : float;
+    var yOffsetNormal : float;
+
+    
+    //Used for fast heart rate
     var framesFast : float = 29;
-    var sizeSmall : float;
-    var sizeNormal : float;
-    var sizeLarge : float;
+    var yStretchFast : float;
+    var yOffsetFast : float;
+    
+    //Used for my start screen hack code. Delete this once we have a real UI.
     var titleScreen : boolean = false;
    var stopAnimatingVar : boolean = false;
+   
+   
+   //These variables allow the thing to change in real time.
    private var currentFrames : float;
-      private var currentSize : float;
+    private var currentYStretch : float;
+    private var currentYOffset : float;
   
     function Start(){  
- currentFrames = framesNormal;
-currentSize = sizeNormal;
+	currentFrames = framesNormal;
+	currentYStretch = yStretchNormal;
+	currentYOffset = yOffsetNormal;
      }
      
     function Update() {
         if(titleScreen)
         {
 
-        //the hackiest hack that ever hacked
+        //the hackiest hack that ever hacked (used for the title screen)
    			if(stopAnimatingVar==false)
    			{
            var index2 : int = (Time.time * framesPerSecond) % frames.Length;
@@ -34,29 +51,44 @@ currentSize = sizeNormal;
             }
             }
         }else{
+        
+        //this is where all the magic is. The hackiest hack that ever hacked was to reuse this script for the
+        //title screen in the worst way ever. Seriously. 
+        
         var index : int = (Time.time * currentFrames) % frames.Length;
         if (this.guiTexture)
         this.guiTexture.texture = frames[index];
         else
         renderer.material.mainTexture = frames[index];
-        //need to stretch the z of this plane over time to equal currentsize whether that be lower or higher
+        
+        //Code needs to be here. Lerp between the Pixel inset Y value of the GUITexture and 
+        //private variabe currentYOffet.
+        
+        //At the same time you need to lerp between the pixel inset height value of the GUITexture and
+        //private variable currentYStretch.
+        
+        //Then, as long as you send this the messages "HeartSlow", "HeartNormal", and "HeartFast" it
+        //should just work on its own.
     }
     }
     
 function HeartSlow()
 {
 currentFrames = framesSlow;
-currentSize = sizeSmall;
+currentYStretch = yStretchSlow;
+currentYOffset = yOffsetSlow;
 }
 
 function HeartNormal()
 {
 currentFrames = framesNormal;
-currentSize = sizeNormal;
+currentYStretch = yStretchNormal;
+currentYOffset = yOffsetNormal;
 }
 
 function HeartFast()
 {
 currentFrames = framesFast;
-currentSize = sizeLarge;
+currentYStretch = yStretchFast;
+currentYOffset = yOffsetFast;
 }
